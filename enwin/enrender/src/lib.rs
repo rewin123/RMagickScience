0964bc8562701a14505b1acaaf181113f9958c3b
rewin <rewin1996@gmail.com>
2021-10-23 11:48:42 +0300

    enh: select physical device

--- enwin/enrender/src/lib.rs
+++ enwin/enrender/src/lib.rs
@@ -19,11 +19,22 @@ mod validation;
 use crate::constants::*;
 use crate::validation::*;
 
+struct QueueFamilyIndices {
+    graphics_family: Option<u32>,
+}
+
+impl QueueFamilyIndices {
+    pub fn is_complete(&self) -> bool {
+        self.graphics_family.is_some()
+    }
+}
+
 pub struct VulkanApp {
     instance : ash::Instance,
     entry : ash::Entry,
     debug_utils_loader: ash::extensions::ext::DebugUtils,
-    debug_merssager: vk::DebugUtilsMessengerEXT
+    debug_merssager: vk::DebugUtilsMessengerEXT,
+    _physical_device: vk::PhysicalDevice,
 }
 
 impl VulkanApp {
@@ -32,13 +43,159 @@ impl VulkanApp {
             let entry = ash::Entry::new().unwrap();
             let instance = VulkanApp::create_instance(&entry, glfw);
             let (debug_utils_loader, debug_merssager) = VulkanApp::setup_debug_utils(&entry, &instance);
+
+            let physical_device = VulkanApp::pick_physical_device(&instance);
             Self {
                 entry,
                 instance,
                 debug_utils_loader,
                 debug_merssager,
+                _physical_device: physical_device
             }
         }
+    }
+
+    fn pick_physical_device(instance: &ash::Instance) -> vk::PhysicalDevice {
+        let physical_devices = unsafe {
+            instance
+                .enumerate_physical_devices()
+                .expect("Failed to enumerate Physical Devices!")
+        };
+
+        println!(
+            "{} devices (GPU) found with vulkan support.",
+            physical_devices.len()
+        );
+
+        let mut result = None;
+        for &physical_device in physical_devices.iter() {
+            if VulkanApp::is_physical_device_suitable(instance, physical_device) {
+                if result.is_none() {
+                    result = Some(physical_device)
+                }
+            }
+        }
+
+        match result {
+            None => panic!("Failed to find a suitable GPU!"),
+            Some(physical_device) => physical_device,
+        }
+    }
+
+    fn is_physical_device_suitable(
+        instance: &ash::Instance,
+        physical_device: vk::PhysicalDevice,
+    ) -> bool {
+        let device_properties = unsafe { instance.get_physical_device_properties(physical_device) };
+        let device_features = unsafe { instance.get_physical_device_features(physical_device) };
+        let device_queue_families =
+            unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
+
+        let device_type = match device_properties.device_type {
+            vk::PhysicalDeviceType::CPU => "Cpu",
+            vk::PhysicalDeviceType::INTEGRATED_GPU => "Integrated GPU",
+            vk::PhysicalDeviceType::DISCRETE_GPU => "Discrete GPU",
+            vk::PhysicalDeviceType::VIRTUAL_GPU => "Virtual GPU",
+            vk::PhysicalDeviceType::OTHER => "Unknown",
+            _ => panic!(),
+        };
+
+        let device_name = vk_to_string(&device_properties.device_name);
+        println!(
+            "\tDevice Name: {}, id: {}, type: {}",
+            device_name, device_properties.device_id, device_type
+        );
+
+        let major_version = vk::version_major(device_properties.api_version);
+        let minor_version = vk::version_minor(device_properties.api_version);
+        let patch_version = vk::version_patch(device_properties.api_version);
+
+        println!(
+            "\tAPI Version: {}.{}.{}",
+            major_version, minor_version, patch_version
+        );
+
+        println!("\tSupport Queue Family: {}", device_queue_families.len());
+        println!("\t\tQueue Count | Graphics, Compute, Transfer, Sparse Binding");
+        for queue_family in device_queue_families.iter() {
+            let is_graphics_support = if queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+            {
+                "support"
+            } else {
+                "unsupport"
+            };
+            let is_compute_support = if queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE) {
+                "support"
+            } else {
+                "unsupport"
+            };
+            let is_transfer_support = if queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER)
+            {
+                "support"
+            } else {
+                "unsupport"
+            };
+            let is_sparse_support = if queue_family
+                .queue_flags
+                .contains(vk::QueueFlags::SPARSE_BINDING)
+            {
+                "support"
+            } else {
+                "unsupport"
+            };
+
+            println!(
+                "\t\t{}\t    | {},  {},  {},  {}",
+                queue_family.queue_count,
+                is_graphics_support,
+                is_compute_support,
+                is_transfer_support,
+                is_sparse_support
+            );
+        }
+
+        // there are plenty of features
+        println!(
+            "\tGeometry Shader support: {}",
+            if device_features.geometry_shader == 1 {
+                "Support"
+            } else {
+                "Unsupport"
+            }
+        );
+
+        let indices = VulkanApp::find_queue_family(instance, physical_device);
+
+        return indices.is_complete();
+    }
+
+    fn find_queue_family(
+        instance: &ash::Instance,
+        physical_device: vk::PhysicalDevice,
+    ) -> QueueFamilyIndices {
+        let queue_families =
+            unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
+
+        let mut queue_family_indices = QueueFamilyIndices {
+            graphics_family: None,
+        };
+
+        let mut index = 0;
+        for queue_family in queue_families.iter() {
+            if queue_family.queue_count > 0
+                && queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+            {
+                queue_family_indices.graphics_family = Some(index);
+            }
+
+            if queue_family_indices.is_complete() {
+                break;
+            }
+
+            index += 1;
+        }
+
+        queue_family_indices
     }
 
     fn setup_debug_utils(
