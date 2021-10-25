57c44c23c0abe6aeda3c4fe4d3fd70a5928dc3d4
Andrey Yamaev <a.yamaev@smartengines.com>
2021-10-25 12:08:35 +0300

    fix: remove vulkan code to paste wgpu

--- enwin/src/lib.rs
+++ enwin/src/lib.rs
@@ -1,8 +1,6 @@
-extern crate glfw;
 use std::sync::mpsc::Receiver;
 use encore::*;
 
-use ash::vk_make_version;
 use ash::vk;
 use std::ffi::CString;
 use std::ptr;
@@ -15,89 +13,20 @@ use ash::extensions::khr::Surface;
 
 #[cfg(target_os = "windows")]
 use ash::extensions::khr::Win32Surface;
-
-struct VulkanApp {
-    instance : ash::Instance,
-    entry : ash::Entry
-}
-
 pub struct Window {
-    win : glfw::Window,
-    events : Receiver<(f64, glfw::WindowEvent)>
+
 }
 
 pub struct Game {
     logick : Logic,
     window : Window,
-    glfw : Glfw,
-    vulkan_app : VulkanApp
-}
-
-
-impl VulkanApp {
-    pub fn new(glfw : &mut glfw::Glfw) -> VulkanApp {
-        let entry = ash::Entry::new().unwrap();
-        let instance = VulkanApp::create_instance(&entry, &mut glfw);
-        Self {
-            entry,
-            instance
-        }
-    }
-
-    fn create_instance(
-        entry : &ash::Entry,
-        glfw : &mut glfw::Glfw
-    ) -> ash::Instance {
-        let app_name = CString::new("Hello app").unwrap();
-        let engine_name = CString::new("Rewin engine").unwrap();
-        let app_info = vk::ApplicationInfo {
-            s_type : vk::StructureType::APPLICATION_INFO,
-            p_next : ptr::null(),
-            p_application_name : app_name.as_ptr(),
-            p_engine_name : engine_name.as_ptr(),
-            application_version : vk_make_version!(1, 0, 0),
-            engine_version : vk_make_version!(1, 0, 0),
-            api_version : vk_make_version!(1, 0, 92)
-        };
-
-        let extension_names = required_extension_names();
-
-        let create_info = vk::InstanceCreateInfo {
-            s_type: vk::StructureType::INSTANCE_CREATE_INFO,
-            p_next: ptr::null(),
-            flags: vk::InstaceCreateFlags().empty(),
-            p_application_info: &app_info,
-            pp_enabled_layer_names: ptr::null(),
-            enabled_layer_count: 0,
-            pp_enabled_extension_names: extension_names.as_ptr(),
-            enabled_extension_count: extension_names.len() as u32
-        };
-
-        let instance: ash::Instance = unsafe {
-            entry
-                .create_instance(&create_info, None)
-                .expect("Failed to create instance!")
-        };
-
-        instance
-    }
 }
 
 impl Window {
-    pub fn new(glfw : &mut glfw::Glfw) -> Window {
-        let (mut window, events) = glfw.create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
-            .expect("Failed to create GLFW window.");
-        window.make_current();
-        let required_extensions = glfw.get_required_instance_extensions().unwrap_or(vec![]);
-        println!("Vulkan required extensions: {:?}", required_extensions);
-        Self {
-            win : window,
-            events
-        }
-    }
+    pub fn new() -> Window {
 
-    pub fn should_close(&self) -> bool {
-        self.win.should_close()
+        Self {
+        }
     }
 
 
@@ -105,22 +34,15 @@ impl Window {
 
 impl Game {
     pub fn new() -> Game {
-        let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-        let mut win = Window::new(&mut glfw);
-        let mut app = VulkanApp::new(&mut glfw);
+        let mut win = Window::new();
         Self {
             window : win,
-            glfw,
             logick : Logic::new(),
-            vulkan_app : app
         }
     }
 
     pub fn game_loop(&mut self) {
-        while !self.window.should_close() {
-            self.glfw.poll_events();
 
-        }
     }
 }
 
