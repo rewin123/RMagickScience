e922ffa4e180aa0a8fb50dbb49ac083356c33251
Andrey Yamaev <a.yamaev@smartengines.com>
2021-10-26 18:51:57 +0300

    enh: basic of resource system

--- enwin/encore/src/lib.rs
+++ enwin/encore/src/lib.rs
@@ -12,7 +12,7 @@ pub enum ResState {
 
 pub trait Resource<T> {
     fn GetState(&self) -> ResState;
-    fn new(path : String) -> Arc<Resource<T>> where Self: Sized;
+    fn new(path : &String) -> Arc<Resource<T>> where Self: Sized;
 
 }
 
@@ -26,10 +26,10 @@ impl Resource<StringResource> for StringResource {
         todo!()
     }
 
-    fn new(path: String) -> Arc<Resource<StringResource>> where Self: Sized {
+    fn new(path: &String) -> Arc<Resource<StringResource>> where Self: Sized {
         Arc::new(
             Self {
-                path,
+                path : String::clone(path),
                 state : InMemory
             })
     }
@@ -61,4 +61,16 @@ impl ResourceManager {
             resource_table : HashMap::new()
         }
     }
+
+    // pub fn get<T>(self, path : &String) -> Arc<dyn Resource<T>> where T : Sized {
+    //     // let res = self.resource_table.get(path);
+    //     // match res {
+    //     //     Some(val) => Arc::clone(val) as Arc<dyn Resource<T>>,
+    //     //     None => {
+    //     //         let arc = Resource::<T>::new(path);
+    //     //         self.resource_table.insert(path, arc);
+    //     //         Arc::clone(&arc)
+    //     //     }
+    //     // }
+    // }
 }
